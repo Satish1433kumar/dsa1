9eb1cca6f3ba23d7f8d872e371dfe19a1e2f886b
PABBATHI SATISH KUMAR <118808994+Satish1433kumar@users.noreply.github.com>
2024-04-13 12:00:49 +0530

    basic problems solved in java

--- Matrices.java
+++ Matrices.java
@@ -328,13 +328,13 @@ public static void getRowAndColumnSums(int a[][]){
     }
     
 public static void main(String args[]){
-    // int[][] a = {
-    //       {1, 2, 3, 4, 5},
-    //       {6, 7, 8, 9, 10},
-    //       {11, 12, 13, 14, 15},
-    //       {16, 17, 18, 19, 20},
-    //       {21, 22, 23, 24, 25}
-    //   };
+    int[][] a = {
+          {1, 2, 3, 4, 5},
+          {6, 7, 8, 9, 10},
+          {11, 12, 13, 14, 15},
+          {16, 17, 18, 19, 20},
+          {21, 22, 23, 24, 25}
+      };
     // int[][] b = {
     //     {1, 2, 3, 4, 5},
     //     {6, 7, 8, 9, 10},
@@ -350,7 +350,7 @@ public static void main(String args[]){
         //9)printUpperTriangleMatrix(a);
         //10)getRowAndColumnSums(a);
         //2)multiplyMatrices(a, b);
-        //11)boundaryTraversal(a);
+        boundaryTraversal(a);
         //12)spiralMatrix(a);
         //13)snakePattern(a);
         //14)clockWiseRotationOfMatrx(a);
